85fd101252cd875a7ef474c195ace2a5497d5f30
DESKTOP-KVV5GLK\HUY <dqhuy711@gmail.com>
2021-11-29 15:06:50 +0700

    Admin Commit 1

--- WEBFILM/Controllers/HomeController.cs
+++ WEBFILM/Controllers/HomeController.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net;
 using System.Web.Mvc;
+using System.Net.Mail;
+using WEBFILM.Models;
 
 namespace WEBFILM.Controllers
 {
     public class HomeController : Controller
     {
+        MovieDataContext db = new MovieDataContext();
         public ActionResult Index()
         {
             return View();
@@ -17,10 +21,6 @@ namespace WEBFILM.Controllers
             return View();
         }
         public ActionResult Detail()
-        {
-            return View();
-        }
-        public ActionResult Login()
         {
             return View();
         }
@@ -36,6 +36,50 @@ namespace WEBFILM.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            var un = collection["Username"];
+            var pw = collection["Password"];
+            if (String.IsNullOrEmpty(un))
+                ViewData["Loi"] = "Bạn phải nhập tên đăng nhập";
+            else if (String.IsNullOrEmpty(pw))
+            {
+                ViewData["Loi1"] = "Bạn phải nhập mật khẩu";
+            }
+            else
+            {
+                User tk = db.Users.SingleOrDefault(n => n.Username.Equals(un) && n.Password.Equals(pw));
+                if (tk != null)
+                {
+                    if (tk.Permission == true)//Admin
+                    {
+                        @Session["UserName"] = tk.Username;
+                        @Session["Permission"] = 1;
+                        ViewBag.ThongBao = "Đăng nhập Amin thành công admin";
+                        return RedirectToAction("Home", "Admin");
+                    }
+                    if (tk.Permission == false || tk.Permission == null)
+                    {
+                        @Session["UserName"] = tk.Username;
+                        @Session["Permission"] = null;
+                        ViewBag.ThongBao = "Đăng nhập thành công";
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ViewData["Loi2"] = "Tên đăng nhâp hoặc mật khẩu không đúng";
+                }
+            }
+
+            return RedirectToAction("Login", "User");
         }
     }
 }
